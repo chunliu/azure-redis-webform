4c81d7ea9dd640889fb41b41161d256f66865fdf
Chun Liu <chunliu@microsoft.com>
2019-03-08 13:18:39 +0800

    Add all code

---
+++ azure-redis-webform/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="azure_redis_webform.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Redis SessionState Provider</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Redis SessionState Provider</h2>
+        <p>
+            This value is from Redis SessionState Provider. This session starts at: <%= Session["redis"] %>
+        </p>
+        <p>
+            Current time: <%= DateTime.Now.ToString() %>
+        </p>
+        <p>
+            <div>
+                Redis cache key: <asp:Label ID="keyLabel" runat="server" />
+            </div>
+            <div>
+                Redis cache value: <asp:Label ID="valueLabel" runat="server" />
+            </div>
+        </p>
+    </form>
+</body>
+</html>
---
+++ azure-redis-webform/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace azure_redis_webform
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private Lazy<ConnectionMultiplexer> lazyConnection = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["redis"] == null)
+                Session["redis"] = DateTime.Now.ToString();
+
+            if (lazyConnection == null)
+            {
+                lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+                {
+                    var hostname = ConfigurationManager.AppSettings["hostname"];
+                    var accesskey = ConfigurationManager.AppSettings["accesskey"];
+                    var useSsl = bool.Parse(ConfigurationManager.AppSettings["sslkey"]);
+
+                    var options = new ConfigurationOptions();
+                    options.EndPoints.Add(hostname);
+                    options.Password = accesskey;
+                    options.Ssl = useSsl;
+                    options.AbortOnConnectFail = false;
+
+                    return ConnectionMultiplexer.Connect(options);
+                });
+            }
+
+            if (!int.TryParse(ConfigurationManager.AppSettings["databaseid"], out int databaseid))
+            {
+                databaseid = 0;
+            }
+
+            IDatabase cache = lazyConnection.Value.GetDatabase(databaseid);
+            var reqHost = cache.StringGet("WebHostName");
+            if (string.IsNullOrEmpty(reqHost))
+            {
+                reqHost = HttpContext.Current.Request.Url.Host;
+                cache.StringSet("WebHostName", reqHost);
+            }
+
+            this.keyLabel.Text = "WebHostName";
+            this.valueLabel.Text = reqHost;
+        }
+    }
+}
